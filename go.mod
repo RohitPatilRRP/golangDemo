6f18a19757318f637377388dcc8c419147c6259b
Rohit Patil <rohit3.patil@ril.com>
2021-08-19 21:15:19 +0530

    Adding crud apis & swagger

--- go.mod
+++ go.mod
@@ -2,4 +2,31 @@ module github.com/RohitPatilRRP/golangDemo
 
 go 1.16
 
-require github.com/gorilla/mux v1.8.0 // indirect
+require (
+	github.com/alecthomas/template v0.0.0-20190718012654-fb15b899a751 // indirect
+	github.com/cpuguy83/go-md2man/v2 v2.0.1 // indirect
+	github.com/felixge/httpsnoop v1.0.2 // indirect
+	github.com/fsnotify/fsnotify v1.5.0 // indirect
+	github.com/go-openapi/analysis v0.20.1 // indirect
+	github.com/go-openapi/jsonreference v0.19.6 // indirect
+	github.com/go-openapi/runtime v0.19.30 // indirect
+	github.com/go-openapi/swag v0.19.15 // indirect
+	github.com/go-stack/stack v1.8.1 // indirect
+	github.com/go-swagger/go-swagger v0.27.0 // indirect
+	github.com/gorilla/mux v1.8.0 // direct
+	github.com/kr/pretty v0.3.0 // indirect
+	github.com/mailru/easyjson v0.7.7 // indirect
+	github.com/rogpeppe/go-internal v1.8.0 // indirect
+	github.com/spf13/cast v1.4.1 // indirect
+	github.com/spf13/viper v1.8.1 // indirect
+	github.com/swaggo/files v0.0.0-20210815190702-a29dd2bc99b2 // indirect
+	github.com/swaggo/gin-swagger v1.3.1 // indirect
+	github.com/swaggo/http-swagger v1.1.1 // indirect
+	github.com/swaggo/swag v1.7.1 // indirect
+	go.mongodb.org/mongo-driver v1.7.1 // indirect
+	golang.org/x/mod v0.5.0 // indirect
+	golang.org/x/net v0.0.0-20210813160813-60bc85c4be6d // indirect
+	golang.org/x/sys v0.0.0-20210819135213-f52c844e1c1c // indirect
+	golang.org/x/text v0.3.7 // indirect
+	golang.org/x/tools v0.1.5 // indirect
+)
--- main.go
+++ main.go
@@ -1,15 +1,133 @@
 package main
 
+// @title Books API documentation
+// @version 1.0.0
+// @host localhost:5000
+// @BasePath /api/books
+
 import (
 	"encoding/json"
 	"log"
-	"net/http"
 	"math/rand"
-	"mux"
+	"net/http"
 	"strconv"
-	"fmt"
+	"github.com/gorilla/mux"
+	httpSwagger "github.com/swaggo/http-swagger"
+	_ "github.com/RohitPatilRRP/golangDemo/docs"	
 )
 
+//Book struct (Model)
+type Book struct {
+	ID     string  `json:"id"`
+	Isbn   string  `json:"isbn"`
+	Title  string  `json:"title"`
+	Aurhor *Author `json:"author"`
+}
+
+//Author struct (Model)
+type Author struct {
+	Firstname string `json:"firstname"`
+	Lastname  string `json:"lastname"`
+}
+
+// Init books var as a slice book struct
+var books []Book
+
+// GetBooks ... Get all books
+// @Summary Get all books
+// @Description get all books
+// @Tags Books
+// @Success 200 {array} Book
+// @Failure 404 {object} object
+// @Router / [get]
+func getBooks(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(books)
+}
+
+// GET single book
+func getBook(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for _, item := range books {
+		if item.ID == params["id"] {
+			json.NewEncoder(w).Encode(item)
+			return
+		}
+	}
+	json.NewEncoder(w).Encode(&Book{})
+}
+
+// CreateBook... Create Book
+// @Summary Create new book based on paramters
+// @Description Create new book
+// @Tags Books
+// @Accept json
+// @Param user body Book true "Book Data"
+// @Success 200 {object} object
+// @Failure 400,500 {object} object
+// @Router / [post]
+func createBook(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	var book Book
+	_ = json.NewDecoder(r.Body).Decode(&book)
+	book.ID = strconv.Itoa(rand.Intn(10000000))
+	books = append(books, book)
+	json.NewEncoder(w).Encode(book)
+}
+
+// Update book
+func updateBook(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for index, item := range books {
+		if item.ID == params["id"] {
+			books = append(books[:index], books[index+1:]...)
+			var book Book
+			_ = json.NewDecoder(r.Body).Decode(&book)
+			book.ID = params["id"]
+			books = append(books, book)
+			json.NewEncoder(w).Encode(book)
+			return
+		}
+	}
+	json.NewEncoder(w).Encode(books)
+}
+
+// Delete Book
+func deleteBook(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for index, item := range books {
+		if item.ID == params["id"] {
+			books = append(books[:index], books[index+1:]...)
+			break
+		}
+	}
+	json.NewEncoder(w).Encode(books)
+}
+
 func main() {
-    fmt.Println("Hello, World!")
+
+	r := mux.NewRouter()
+
+	//Mock Data
+	books = append(books, Book{ID: "1", Isbn: "123", Title: "Book One", Aurhor: &Author{Firstname: "Rohit", Lastname: "Patil"}})
+	books = append(books, Book{ID: "2", Isbn: "132", Title: "Book Two", Aurhor: &Author{Firstname: "John", Lastname: "Miller"}})
+
+	r.HandleFunc("/api/books/", getBooks).Methods("GET")
+	r.HandleFunc("/api/books/{id}", getBook).Methods("GET")
+	r.HandleFunc("/api/books", createBook).Methods("POST")
+	r.HandleFunc("/api/books/{id}", updateBook).Methods("PUT")
+	r.HandleFunc("/api/books/{id}", deleteBook).Methods("DELETE")
+
+	r.PathPrefix("/swagger/").Handler(httpSwagger.Handler(
+		httpSwagger.URL("http://localhost:8080/swagger/doc.json"),//The url pointing to API definition
+		httpSwagger.DeepLinking(true),
+		httpSwagger.DocExpansion("none"),
+		httpSwagger.DomID("#swagger-ui"),
+	))
+
+	log.Fatal(http.ListenAndServe(":8080", r))
+
 }
